f5ab5a1d8aaf1dbe53e37e1a0fa45b9e8fda7a63
Glaucoed <gesverzuthe@gmail.com>
2022-06-21 18:49:18 -0300

    exercicio 2 realizado

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercices-pratice.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercices-pratice.js
@@ -1,11 +1,36 @@
 // Exercicio 01
 
-const a = 15
-const b = 20
-let result = 0
+// const a = 15;
+// const b = 20;
+// let result = 0;
 
-console.log(let = a + b)
-console.log(let = a - b)
-console.log(let = a * b)
-console.log(let = a / b)
-console.log(let = a % b)
+// console.log(let = a + b);
+// console.log(let = a - b);
+// console.log(let = a * b);
+// console.log(let = a / b);
+// console.log(let = a % b);
+
+// Exercicio 02
+
+const a = 15;
+const b = 20;
+let maior = 0;
+
+if(a > b){
+    console.log("A é o maior")
+
+}
+else{
+    console.log("B é o maior")
+}
+
+// Exercicio 03
+
+
+
+
+
+// Exercicio 04
+
+
+
