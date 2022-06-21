955ed3c743c462f0624a69ef55b1f81e6976143d
Glaucoed <gesverzuthe@gmail.com>
2022-06-21 19:10:40 -0300

    exercicio 3 realizado

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercices-pratice.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercices-pratice.js
@@ -12,22 +12,32 @@
 
 // Exercicio 02
 
-const a = 15;
-const b = 20;
-let maior = 0;
+// const a = 15;
+// const b = 20;
+// let maior = 0;
 
-if(a > b){
-    console.log("A é o maior")
-
-}
-else{
-    console.log("B é o maior")
-}
+// if(a > b){
+//     console.log("A é o maior")
+// }
+// else{
+//     console.log("B é o maior")
+// }
 
 // Exercicio 03
 
+const a = 25;
+const b = 20;
+const c = 5;
 
-
+if (a > b && a > c){
+    console.log("O numero maior é: " + a + " A");
+}
+else if(b > a && b > c){
+    console.log("O numero maior é: " + b + " B");
+}
+    else{
+    console.log("O numero maior é: " + c + " C");
+};
 
 
 // Exercicio 04
