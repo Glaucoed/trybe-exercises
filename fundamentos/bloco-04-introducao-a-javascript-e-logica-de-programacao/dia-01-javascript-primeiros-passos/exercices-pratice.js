b64e7916bd1334f3b8277cdb8e0633846e57c0a3
Glaucoed <gesverzuthe@gmail.com>
2022-06-21 18:43:26 -0300

    exercicio 1 realizado

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercices-pratice.js
@@ -0,0 +1,11 @@
+// Exercicio 01
+
+const a = 15
+const b = 20
+let result = 0
+
+console.log(let = a + b)
+console.log(let = a - b)
+console.log(let = a * b)
+console.log(let = a / b)
+console.log(let = a % b)
