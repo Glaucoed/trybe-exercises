23eeb1706909e7da09d223e813497ea789c48820
Glaucoed <gesverzuthe@gmail.com>
2022-06-22 02:20:16 -0300

    exercicio 06 realizado

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercices-pratice.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercices-pratice.js
@@ -56,32 +56,48 @@
 
 // Exercicio 05
 
-const ladoUm = 50;
-const ladoDois = 70;
-const ladoTres = 60;
+// const ladoUm = 50;
+// const ladoDois = 70;
+// const ladoTres = 60;
 
-let triangulo = ladoUm + ladoDois + ladoTres;
+// let triangulo = ladoUm + ladoDois + ladoTres;
 
-let todosLados = ladoUm > 0 && ladoDois > 0 && ladoTres > 0;
+// let todosLados = ladoUm > 0 && ladoDois > 0 && ladoTres > 0;
 
-if(todosLados){
-if (triangulo === 180) {
-    console.log("true")
-} else{
-    console.log("false")
+// if(todosLados){
+// if (triangulo === 180) {
+//     console.log("true")
+// } else{
+//     console.log("false")
+// };
+// }else{
+//     console.log("Algo está errado!")
+// }
+
+// Exercicio 06
+
+let pecas = "Peao"
+
+switch (pecas.toLowerCase()) {
+    case "peao":
+        console.log("Peão -> Frente o primeiro movimento podem ser duas casas.");
+        break;
+    case "cavalo":
+        console.log("Cavalo -> 'L' pode pular sobre as peças");
+        break;
+    case "torre":
+        console.log("Torre -> Vertical ou horizontal");
+        break;
+    case "bispo":
+        console.log("Bispo-> Diagonal");
+        break;
+    case "rainha":
+        console.log("Rainha-> Horizontal, vertical e diagonais");
+        break;
+    case "rei":
+        console.log(" Rei-> Horizontal, vertical e diagonal");
+        break;
+    default:
+        console.log("Peça invalida, tente outra.");  
+
 };
-}else{
-    console.log("Algo está errado!")
-}
-
-
-
-
-
-
-
-
-
-
-
-
