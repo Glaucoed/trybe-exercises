65e0a4036efe793fe4eeced7baa59da68cbceb3c
Glaucoed <gesverzuthe@gmail.com>
2022-06-22 01:25:17 -0300

    Exercicio 05 feito com auxilio do gabarito, sempre revisar

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercices-pratice.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercices-pratice.js
@@ -25,32 +25,61 @@
 
 // Exercicio 03
 
-const a = 25;
-const b = 20;
-const c = 5;
+// const a = 25;
+// const b = 20;
+// const c = 5;
 
-if (a > b && a > c){
-    console.log("O numero maior é A");
-}
-else if(b > a && b > c){
-    console.log("O numero maior é B");
-}
-    else{
-    console.log("O numero maior é C");
-};
+// if (a > b && a > c){
+//     console.log("O numero maior é A");
+// }
+// else if(b > a && b > c){
+//     console.log("O numero maior é B");
+// }
+//     else{
+//     console.log("O numero maior é C");
+// };
+
+
 
 // Exercicio 04
 
-const value = 0;
+// const value = 0;
 
-if(value > 0){
-console.log("positive");
-}
-else if(value < 0){
-console.log("negative");
-}else{
-    console.log("zero");
+// if(value > 0){
+// console.log("positive");
+// }
+// else if(value < 0){
+// console.log("negative");
+// }else{
+//     console.log("zero");
+// };
+
+// Exercicio 05
+
+const ladoUm = 50;
+const ladoDois = 70;
+const ladoTres = 60;
+
+let triangulo = ladoUm + ladoDois + ladoTres;
+
+let todosLados = ladoUm > 0 && ladoDois > 0 && ladoTres > 0;
+
+if(todosLados){
+if (triangulo === 180) {
+    console.log("true")
+} else{
+    console.log("false")
 };
+}else{
+    console.log("Algo está errado!")
+}
+
+
+
+
+
+
+
 
 
 
