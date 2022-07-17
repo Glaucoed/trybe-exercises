8bde6abdee4c8751ef3e0dcd80f9137bbc0ef11d
Glaucoed <gesverzuthe@gmail.com>
2022-07-17 14:46:26 -0300

    realizado part2

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/objetos-keys.js
@@ -0,0 +1,142 @@
+const customer1 = {
+    firstName: 'Roberto',
+    age: 22,
+    job: 'Teacher',
+  };
+  
+  console.log(customer1);
+  
+  customer1.lastName = 'Faria';
+  console.log(customer1);
+  
+  const customer2 = {
+    firstName: 'Maria',
+    age: 23,
+    job: 'Medic',
+  };
+  
+  console.log(customer2);
+  customer2['lastName'] = 'Silva';
+  console.log(customer2);
+
+//objeto.nomeDaPropriedade.
+//objeto[variavelQueContemONomeDaPropriedade].
+
+const customer = {
+    firstName: 'Roberto',
+    age: 22,
+    job: 'Teacher',
+  };
+  
+  let newKey = 'lastName';
+  const lastName = 'Ferreira';
+  customer[newKey] = lastName;
+  newKey = 'fullName';
+  const fullName = `${customer.firstName} ${customer.lastName}`;
+  customer[newKey] = fullName;
+  console.log(customer);
+
+// ===========================================PRECISA CRIAR O OBJETO ABAIXO ========================================================
+
+
+const createKey = (object, name, value) =>{
+    const object = {};
+    let newKey = name;
+    const valueKey = value;
+    object[newKey] = valueKey;
+}
+createKey(MeuObjeto, chaveObjeto, valor);
+
+// ======================================================================================================================================
+
+// listando com o for 
+const coolestTvShow = {
+  name: "BoJack Horseman",
+  genre: "adult animation",
+  author: "Raphael Bob-Waksberg",
+  favoriteCharacter: "Princess Carolyn",
+  quote: "Princess Carolyn always lands on her feet.",
+  seasons: 6,
+};
+
+for (const property in coolestTvShow) {
+  console.log(property);
+}
+
+// name
+// genre
+// author
+// favoriteCharacter
+// quote
+// seasons
+
+
+//
+// Listando de forma dimanica
+const coolestTvShow = {
+  name: "BoJack Horseman",
+  genre: "adult animation",
+  createdBy: "Raphael Bob-Waksberg",
+  favoriteCharacter: "Princess Carolyn",
+  quote: "Princess Carolyn always lands on her feet.",
+  seasons: 6,
+};
+
+// for (const property in coolestTvShow) {
+//   console.log(property);
+// }
+
+console.log(Object.keys(coolestTvShow));
+
+// [ 'name', 'genre', 'createdBy', 'favoriteCharacter', 'quote', 'seasons' ]
+
+/* ========================= Exemplos da documentação ====================================
+var arr = ['a', 'b', 'c'];
+console.log(Object.keys(arr));
+
+var obj = { 0: 'a', 
+            1: 'b', 
+            2: 'c' 
+          };
+console.log(Object.keys(obj)); // console: ['0', '1', '2']
+
+var an_obj = { 100: 'a', 
+               2: 'b',
+               7: 'c'
+              };
+console.log(Object.keys(an_obj)); // console: ['2', '7', '100']
+
+var my_obj = Object.create({}, { getFoo: { value: function() { return this.foo; } } });
+my_obj.foo = 1;
+
+console.log(Object.keys(my_obj)); // console: ['foo']
+*/
+
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const listSkills = (student) => {
+  const arrayOfSkills = Object.keys(student);
+    for (const index in arrayOfSkills) {
+    console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+  }
+};
+
+console.log('Estudante 1');
+listSkills(student1);
+
+console.log('Estudante 2');
+listSkills(student2);
+ 
