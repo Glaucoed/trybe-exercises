73c26cdbb53773b2cbec57a51d39758a6333a1de
Glaucoed <gesverzuthe@gmail.com>
2022-07-24 19:46:28 -0300

    relizado 1 e 2 exercicio

---
+++ fundamentos/bloco-8-higher-order-functions-do-javaScript-ES6/8.2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/ex-foguete.js
@@ -0,0 +1,81 @@
+const books = [
+  {
+    id: 1,
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
+    author: {
+      name: "George R. R. Martin",
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
+    author: {
+      name: "J. R. R. Tolkien",
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: "Fundação",
+    genre: "Ficção Científica",
+    author: {
+      name: "Isaac Asimov",
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: "Duna",
+    genre: "Ficção Científica",
+    author: {
+      name: "Frank Herbert",
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: "A Coisa",
+    genre: "Terror",
+    author: {
+      name: "Stephen King",
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
+    author: {
+      name: "H. P. Lovecraft",
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+function authorBornIn1947() {
+  return books.find((book) => book.author.birthYear === 1947).author.name;
+}
+// console.log(authorBornIn1947());
+
+// return books.find  -> [preciso usar o return para mostrar o meu resultado]
+// ((book) => book.author.birthYear === 1947) -> [Ele vai executar a minha condição e me retornar]
+// .author.name  -> [É o que eu quero mostrar no caso o "autor"]
+// Linha 67 - authorBornIn1947() - preciso chamar a função
+
+function smallerName() {
+  let nameBook;
+  books.forEach((small) => {
+    if (!nameBook || small.name.length < nameBook.length) nameBook = small.name;
+  });
+  return nameBook;
+}
+console.log(smallerName());
