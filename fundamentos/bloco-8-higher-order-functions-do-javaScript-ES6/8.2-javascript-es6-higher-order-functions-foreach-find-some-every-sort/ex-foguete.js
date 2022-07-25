b12fe6557cf762721f0550ddfc0330ecf263fa1f
Glaucoed <gesverzuthe@gmail.com>
2022-07-25 13:18:41 -0300

    relizado 6  exercicio

--- fundamentos/bloco-8-higher-order-functions-do-javaScript-ES6/8.2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/ex-foguete.js
+++ fundamentos/bloco-8-higher-order-functions-do-javaScript-ES6/8.2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/ex-foguete.js
@@ -108,4 +108,10 @@ function everyoneWasBornOnSecXX() {
 function someBookWasReleaseOnThe80s() {
     return books.some((book) => book.releaseYear >= 1980 && book.releaseYear <= 1989)
 }
-console.log(someBookWasReleaseOnThe80s());
+// console.log(someBookWasReleaseOnThe80s());
+
+// Exercicio 07 - const expectedResult = false;
+function authorUnique() {
+    return books.every((book) => !books.some((bookSome) => (bookSome.author.birthYear === book.author.birthYear) && (bookSome.author.name !== book.author.name)));
+  }
+console.log(authorUnique());
