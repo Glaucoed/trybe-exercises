b49723287c340ebca590febdfb594138053d98f0
Glaucoed <gesverzuthe@gmail.com>
2022-07-27 14:49:55 -0300

    exemplo 03

--- fundamentos/bloco-8-higher-order-functions-do-javaScript-ES6/8.5-javascript-es6-higher-order-functions-spread-operator-parametro-rest-destructuring-e-mais/exemplos.js
+++ fundamentos/bloco-8-higher-order-functions-do-javaScript-ES6/8.5-javascript-es6-higher-order-functions-spread-operator-parametro-rest-destructuring-e-mais/exemplos.js
@@ -1,9 +1,13 @@
+// exemplo 01
 const numbers = [1, 2, 3];
 
 const newArray = [...numbers, 4, 5, 6];
-console.log(newArray); // [ 1, 2, 3, 4, 5, 6 ]
-console.log(numbers ); // [ 1, 2, 3 ]
+// console.log(newArray); // [ 1, 2, 3, 4, 5, 6 ]
+// console.log(numbers ); // [ 1, 2, 3 ]
 
+
+
+// exemplo 02
 
 const spring = ['OUT', 'NOV', 'DEZ'];
 const summer = ['JAN', 'FEV', 'MAR'];
@@ -11,9 +15,18 @@ const fall = ['ABR', 'MAI', 'JUN'];
 const winter = ['JUL', 'AGO', 'SET'];
 
 const months = [...summer, ...fall, ...winter, ...spring];
-console.log(months); /* [
+// console.log(months);
+/* [
   'JAN', 'FEV', 'MAR',
   'ABR', 'MAI', 'JUN',
   'JUL', 'AGO', 'SET',
   'OUT', 'NOV', 'DEZ'
 ] */
+
+
+// exemplo 03
+
+const imc = (peso, altura) => (peso / (altura * altura)).toFixed(2);
+const patientInfo = [60, 1.7];
+
+// console.log(imc(...patientInfo)); // 20.76
