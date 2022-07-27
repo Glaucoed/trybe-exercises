2d017957c495dc743b261f9d5760be183ede6dd4
Glaucoed <gesverzuthe@gmail.com>
2022-07-27 16:06:25 -0300

    exercicio 02

--- fundamentos/bloco-8-higher-order-functions-do-javaScript-ES6/8.5-javascript-es6-higher-order-functions-spread-operator-parametro-rest-destructuring-e-mais/array-destructuring.js
+++ fundamentos/bloco-8-higher-order-functions-do-javaScript-ES6/8.5-javascript-es6-higher-order-functions-spread-operator-parametro-rest-destructuring-e-mais/array-destructuring.js
@@ -18,3 +18,25 @@ const [texto, consolelog] = saudacoes
 consolelog(texto) // com destruturação
 
 // Produza o mesmo resultado acima, porém utilizando array destructuring
+
+// Exercicio 02
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+[comida, animal, bebida] = [bebida, comida, animal];
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+
+
+// Exercicio 03
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+[,,, ...numerosPares] = numerosPares;
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
