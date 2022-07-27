b43135e3655e318ccfe0cedf3f24753498e314b2
Glaucoed <gesverzuthe@gmail.com>
2022-07-27 14:51:49 -0300

    exemplo 05

--- fundamentos/bloco-8-higher-order-functions-do-javaScript-ES6/8.5-javascript-es6-higher-order-functions-spread-operator-parametro-rest-destructuring-e-mais/exemplos.js
+++ fundamentos/bloco-8-higher-order-functions-do-javaScript-ES6/8.5-javascript-es6-higher-order-functions-spread-operator-parametro-rest-destructuring-e-mais/exemplos.js
@@ -40,3 +40,30 @@ const randomNumbers = [57, 8, 5, 800, 152, 74, 630, 98, 40];
 
 // console.log(Math.max(...randomNumbers)); // 800
 // console.log(Math.min(...randomNumbers)); // 5
+
+
+
+// Exemplo 05
+
+const people = {
+    id: 101,
+    name: 'Alysson',
+    age: 25,
+  };
+  
+  const about = {
+    address: 'Av. Getúlio Vargas, 1000',
+    occupation: 'Developer',
+  };
+  
+  const customer = { ...people, ...about };
+  console.log(customer); 
+  
+  /* {
+    id: 101,
+    name: 'Alysson',
+    age: 25,
+    address: 'Av. Getúlio Vargas, 1000',
+    occupation: 'Developer'
+  } */
+  
